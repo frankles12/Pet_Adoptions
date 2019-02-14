ceaf3b1d8f754d9f10e60b72d8ec8dbe40e37050
Frank Schiller <frankles1221@gmail.com>
2019-02-14 11:42:07 -0500

    Added initial project files

---
+++ AdoptionApp/src/App.js
@@ -0,0 +1,40 @@
+const Pet = props => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed)
+  ]);
+};
+
+class App extends React.Component {
+  handleTitleClick() {
+    alert("you clicked the title");
+  }
+
+  render() {
+    return React.createElement("div", {}, [
+      React.createElement(
+        "h1",
+        { onClick: this.handleTitleClick },
+        "Adopt Me!"
+      ),
+      React.createElement(Pet, {
+        name: "Luna",
+        animal: "dog",
+        breed: "Havanese"
+      }),
+      React.createElement(Pet, {
+        name: "Ahri",
+        animal: "cat",
+        breed: "Tabby"
+      }),
+      React.createElement(Pet, {
+        name: "Spike",
+        animal: "turtle",
+        breed: "toroise"
+      })
+    ]);
+  }
+}
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
