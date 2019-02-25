a0911c63065368938851a40a00dc36514b43b858
Frank Schiller <frankles1221@gmail.com>
2019-02-25 17:00:49 -0500

    Communication with API

--- AdoptionApp/src/Pet.js
+++ AdoptionApp/src/Pet.js
@@ -1,15 +1,31 @@
 import React from "react";
 
 
-const Pet = props => {
+class Pet extends React.Component {
+    render() {
+        const { name, animal, breed, media, location } = this.props;
+        let photos = [];
+        if (media && media.photos && media.photos.photo) {
+            photos = media.photos.photo.filter(photo => photo["@size"] === "pn");
+        }
 
-    return (
-        <div>
-            <h1>{ props.name }</h1>
-            <h2>{ props.animal }</h2>
-            <h2>{ props.breed }</h2>
-        </div>
-    );
-};
+        let hero = "http://placecorgi.com/300/300";
+        if (photos[0] && photos[0].value) {
+            hero = photos[0].value;
+        }
+
+        return (
+            <div className="pet">
+                <div className="image-container">
+                    <img src={hero} alt={name} />
+                </div>
+                <div className="info">
+                    <h1>{name}</h1>
+                    <h2>{`${animal} — ${breed} — ${location}`}</h2>
+                </div>
+            </div>
+        );
+    }
+}
 
 export default Pet;
