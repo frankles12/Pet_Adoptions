c90d2c102271dea8d51c22ce6f257d3879e0ce81
Frank Schiller <frankles1221@gmail.com>
2019-02-26 16:57:40 -0500

    Added state for Details component

--- AdoptionApp/src/Details.js
+++ AdoptionApp/src/Details.js
@@ -1,6 +1,37 @@
 import React from 'react';
+import pf from 'petfinder-client';
+
+const petfinder = pf({
+    key: process.env.API_KEY,
+    secret: process.env.API_SECRET
+})
 
 class Details extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            loading: true
+        }
+    }
+    componentWillMount (){
+        petfinder.pet.get({
+            output: "full",
+            id: this.props.id
+        }).then(data => {
+            const pet = data.petfinder.pet;
+            let breed;
+            if (Array.isArray(data.petfinder.pet.breeds.breed)) {
+                breed = data.petfinder.pet.breeds.breed.join(', ');
+            } else {
+                breed = data.petfinder.pet.breeds.breed
+            }
+
+            this.this.setState({
+
+            })
+        })
+    }
     render() {
         return <h1>hi lol</h1>
     }
