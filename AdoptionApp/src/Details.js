1c52eb0839038cc4ffa833b76c3a220ecee3468a
Frank Schiller <frankles1221@gmail.com>
2019-03-01 11:22:11 -0500

    Got component rendering from the api

--- AdoptionApp/src/Details.js
+++ AdoptionApp/src/Details.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import pf from 'petfinder-client';
+import {navigate} from "@reach/router/es/lib/history";
 
 const petfinder = pf({
     key: process.env.API_KEY,
     secret: process.env.API_SECRET
-})
+});
 
 class Details extends React.Component {
     constructor(props) {
@@ -14,26 +15,49 @@ class Details extends React.Component {
             loading: true
         }
     }
-    componentWillMount (){
+    componentDidMount (){
         petfinder.pet.get({
             output: "full",
             id: this.props.id
         }).then(data => {
             const pet = data.petfinder.pet;
             let breed;
-            if (Array.isArray(data.petfinder.pet.breeds.breed)) {
-                breed = data.petfinder.pet.breeds.breed.join(', ');
+            if (Array.isArray(pet.breeds.breed)) {
+                breed = pet.breeds.breed.join(', ');
             } else {
-                breed = data.petfinder.pet.breeds.breed
+                breed = pet.breeds.breed
             }
 
-            this.this.setState({
-
+            this.setState({
+                name: pet.name,
+                animal: pet.animal,
+                location: `${pet.contact.city}, ${pet.contact.state}`,
+                description: pet.description,
+                media: pet.media,
+                breed,
+                loading: false
             })
         })
+            .catch(() => {
+                navigate("/")
+            });
     }
     render() {
-        return <h1>hi lol</h1>
+        if (this.state.loading) {
+            return <h1>Loading...</h1>
+        }
+
+        const { name, animal, breed, location, description } = this.state;
+
+        return (
+            <div className="details">
+                <div>
+                    <h1>{name}</h1>
+                    <h2>{animal} - {breed} - {location}</h2>
+                    <p>{description}</p>
+                </div>
+            </div>
+        )
     }
 }
 
